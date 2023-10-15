33b09bc7e48440dec322c9f8be3eabd9adc53d29
mariogustavoorrillo <gustavo.orrillo@gmail.com>
2023-10-15 13:04:53 +0000

    First commit;

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'remito_preimpreso',
+    'version': '14.0.1',
+    'category': 'Stock',
+    'depends': [
+               'stock',
+                ],
+    'data':[
+       'views/remito_preimpreso_report.xml',
+       'views/remito_preimpreso_report_template.xml',
+    ],
+    'installable' : True,
+    'application' : False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
