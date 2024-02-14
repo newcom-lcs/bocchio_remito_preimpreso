d13f7219f8e399fac6614a36774aec345efb322f
Manuel Nuñez <mnunez@newcom-lcs.com>
2024-02-14 16:41:47 +0000

    migrando carpetas

---
+++ remito_preimpreso/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Remito Preimpreso',
+    'version': '16.0.1',
+    'category': 'Stock',
+    'depends': [
+               'stock',
+                ],
+    'data':[
+       'views/remito_preimpreso_report.xml',
+       'views/remito_preimpreso_report_template.xml',
+    ],
+    'auto_install': False,
+    'installable' : True,
+    'application' : False,
+}
+
